579624a070cb194de7555a72501bd18d324455f3
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2023-05-31 03:32:46 +0300

    Create Basics of Google Earth Engine oriented JavaScript.js

---
+++ Basics of Google Earth Engine oriented JavaScript.js
@@ -0,0 +1,57 @@
+/*
+Topics covered
+2.1. Basic programming concepts
+2.2. Mostly used GEE JS API syntax
+2.3. How to write efficient code guide 
+
+*/
+
+// 2.1. Basic programming concepts
+//Hello world
+
+print('Hello World')
+//String
+'I am Gamze'
+// Variables
+
+var a = '123'
+
+var b = 'Hello World B'
+
+print(b)
+// Numbers
+var c = 12 / 3 + (23-1)
+print('Variable C results', c); print('Hi')
+
+// Lists
+var cities = ['Antalya', 'Atina', 'New York']
+print(cities)
+
+// Objects/Dictionaries
+var country = {
+  'name': 'Turkiye',
+  'name2' : 'Greece',
+  'name3' : 'USA'
+}
+
+print(country)
+// Important syntax
+/*
+2.2. Mostly used GEE JS API syntax
+ee.Image()
+ee.ImageCollection()
+Map.setCenter()
+Map.centerObject()
+Map.addLayer()
+*/
+// 2.3. How to write efficient code guide 
+/*
+
+1) Commenting
+2) putting ; at the end of line code
+
+ Anything you write here will be comment
+
+var ndvi_2010 = NDVI
+
+*/
