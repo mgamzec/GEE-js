8bce50f871c6968d6d21bbfb9b13de644ed1ccdd
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2023-07-29 21:10:49 +0300

    Create Basic JavaScript Syntax.js

---
+++ Basic JavaScript Syntax.js
@@ -0,0 +1,23 @@
+//Basic JavaScript Syntax 
+
+// Hello World!
+print('Hello World!');
+
+// Basic JavaScript data types
+
+// 1. Strings
+
+// Use single (or double) quotes to make a string.
+var testString = 'Google Earth Engine!';
+
+// Use parentheses to pass arguments to functions.
+print(testString);
+
+// 2. Numbers
+// Store a number in a variable.
+var number = 99;
+print('My answer is:', number);
+
+// 3. Lists
+var myList = [0, 1, 2, 3, 4, 5];
+print('List of numbers:', myList);
